49a8903485ca32dc6e02184cd3f28d79462f37d6
mhmitas <mahfuzulmitas@gmail.com>
2024-02-18 10:59:08 +0600

    first commit

---
+++ controlHtml.js
@@ -0,0 +1,9 @@
+const leftColumn = document.getElementById('left-column').childNodes
+// for (const row of leftColumn) {
+//     // row.childNodes[3].style.backgroundColor = 'green'
+//     console.log(row.childNodes[1])
+// }
+
+
+const container = document.querySelector('.same-btn-container')
+console.log(container.childNodes[3])
