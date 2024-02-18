8ff56dbd5a35019b8eb618aec849a697282c0324
mhmitas <mahfuzulmitas@gmail.com>
2024-02-18 20:23:10 +0600

    commit commit

--- app.js
+++ app.js
@@ -18,8 +18,10 @@ for (const seatbtn of seatBtns) {
         const updatedSeatsLeft = seatsLeft - 1;
         setInnerValue('seats-left', updatedSeatsLeft)
 
+        const btnTxt = e.target.innerText
+
         // send seat data to total container 
-        sendDataToSeatContainer()
+        sendDataToSeatContainer(btnTxt)
     })
 }
 
@@ -37,17 +39,20 @@ function setInnerValue(id, value) {
     element.innerText = value
 }
 
-function sendDataToSeatContainer() {
+function sendDataToSeatContainer(text) {
     const seatNameContainer = document.getElementById('seat-name-container')
-    // const seatDiv = document.createElement('div')
+    const seatName = document.createElement('div')
+    seatName.innerText = text;
+    seatNameContainer.appendChild(seatName)
+
+    // seat-class-container
     const seatClassContainer = document.getElementById('seat-class-container')
     const seatClass = document.createElement('div')
     seatClass.innerText = 'Economoy'
     seatClassContainer.appendChild(seatClass)
-
+    // seat-price-container
     const seatPriceContainer = document.getElementById('seat-price-container')
     const seatPrice = document.createElement('div')
     seatPrice.innerText = ticketPrice;
     seatPriceContainer.appendChild(seatPrice)
-    // console.log()
 }
