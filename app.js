27be714fe9b1de9d2a33d727857ffe47816c97c1
mhmitas <mahfuzulmitas@gmail.com>
2024-02-18 18:48:02 +0600

    commit commit

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 const seatBtns = document.getElementsByClassName('seat-num')
 for (const seatbtn of seatBtns) {
     seatbtn.addEventListener('click', function (e) {
-        
+
         // set bg of clicked btn
         const bg = e.target.style.backgroundColor = '#2eb82e';
         const color = e.target.style.color = '#ffffff';
 
+        // update seat quantity
         const seat = getInnerValue('seat')
         const updatedSeat = seat + 1;
-        setInnerValue('seat',updatedSeat)
-
+        setInnerValue('seat', updatedSeat)
+        // update total seat left
         const seatsLeft = getInnerValue('seats-left')
         const updatedSeatsLeft = seatsLeft - 1;
         setInnerValue('seats-left', updatedSeatsLeft)
-        console.log(updatedSeatsLeft)
+
+        // send seat data to total container
+        const seatContainer = document.getElementById('selected-seat-container')
+
+        const parentDiv = document.createElement('div')
+        parentDiv.innerText = 'hello'
+        const seatDiv = document.createElement('div')
+
+        const seatClassDiv = document.createElement('div')
+        seatClassDiv.innerText = 'Economoy'
+        const seatPriceDiv = document.createElement('div')
+        seatPriceDiv.innerText = 550;
+        seatContainer.appendChild(parentDiv)
+        // parentDiv.appendChild(seatDiv)
+        // parentDiv.appendChild(seatClassDiv)
+        // parentDiv.appendChild(seatPriceDiv)
     })
 }
 
@@ -26,7 +42,7 @@ function getInnerValue(id) {
     return value;
 }
 
-function setInnerValue(id, value){
+function setInnerValue(id, value) {
     const element = document.getElementById(id)
     element.innerText = value
 }
